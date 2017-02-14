ba0646933bf6f501fc3eb75a0bf655297f717e21
范佳悦 <fanjy14@mails.tsinghua.edu.cn>
2017-02-14 22:02:34 +0800

    v0.0.1

--- pages/content/content.js
+++ pages/content/content.js
@@ -3,7 +3,8 @@ Page({
     data: {
         word:null,
         message: null,
-        request_fail: false
+        request_fail: false,
+        image_url:null
     },
     onLoad: function(options) {
         console.log(options)
@@ -29,14 +30,28 @@ Page({
                 q: options.word,
             },
             success: function(res) {
-                console.log(res.data)
                 that.setData({
                     message: that.html_encode(res.data)
                 });
             },
             fail: function(error) {
-                console.log("ashfisafsdafadadas");
-                console.log(error);
+                that.setData({
+                    request_fail: true
+                })
+            }
+        })
+
+        wx.request({
+            url: "https://cn.bing.com/image/search",
+            data: {
+                q: options.word,
+            },
+            success: function(res) {
+                that.setData({
+                    image_url: that.html_encode_image(res.data)
+                });
+            },
+            fail: function(error) {
                 that.setData({
                     request_fail: true
                 })
@@ -44,11 +59,22 @@ Page({
         })
     },
     html_encode: function(str) {
-        console.log("faofd"+str);
         var re = new RegExp("<span class=\"def\"><span>([\u0391-\uFFE5]+)</span>","g");
         var str=str.match(re);
         console.log(str);
         str = str[0].replace(/<\/?[^>]*>/g, '');
         return str;
+    },
+    html_encode_image: function(str) {
+        var re = new RegExp("&quot;http://(.*?)&quot","g");
+        var str1=str.match(re);
+        console.log(str1[1]);
+        str1 = str1[1].replace(/&quot/g, '');
+        console.log(str1);
+        //str1 = str1[1].replace(/&amp/g, '');
+        //console.log(str1);
+        str1 = str1.replace(/\;/g, '');
+        console.log(str1);
+        return str1;
     }
 })
