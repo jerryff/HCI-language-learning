2109196884312326bf2fb0bfcb4a3dbc14524b37
范佳悦 <fanjy14@mails.tsinghua.edu.cn>
2017-02-14 10:14:23 +0800

    origin

---
+++ pages/content/content.js
@@ -0,0 +1,54 @@
+var app = getApp()
+Page({
+    data: {
+        word:null,
+        message: null,
+        request_fail: false
+    },
+    onLoad: function(options) {
+        console.log(options)
+        this.setData({
+            word:options.word
+        })
+        var that = this;
+        // 动态设置标题栏。。。无效
+        wx.setNavigationBarTitle({
+            title: options.word,
+            fail: function() {
+                console.log("更改标题失败");
+            },
+            success: function() {
+                console.log("更改标题成功");
+            }
+        });
+
+        //get请求
+        wx.request({
+            url: "https://cn.bing.com/dict/search",
+            data: {
+                q: options.word,
+            },
+            success: function(res) {
+                console.log(res.data)
+                that.setData({
+                    message: that.html_encode(res.data)
+                });
+            },
+            fail: function(error) {
+                console.log("ashfisafsdafadadas");
+                console.log(error);
+                that.setData({
+                    request_fail: true
+                })
+            }
+        })
+    },
+    html_encode: function(str) {
+        console.log("faofd"+str);
+        var re = new RegExp("<span class=\"def\"><span>([\u0391-\uFFE5]+)</span>","g");
+        var str=str.match(re);
+        console.log(str);
+        str = str[0].replace(/<\/?[^>]*>/g, '');
+        return str;
+    }
+})
